5ef71b397e517177588bf17489602f592c4b7c5e
JbRezidu <duriez.jeanbaptiste@gmail.com>
2018-02-13 22:08:03 +0100

    fin authentification

--- api/authentication/authentication.controller.js
+++ api/authentication/authentication.controller.js
@@ -5,8 +5,25 @@ const dbSettings = require('db/index2');
 const Streamer = require('api/streamer/streamer.model');
 const randToken = require('rand-token');
 
+const checkAuthentication = (req, res, next) => {
+  const streamer = req.headers.streamer;
+  const token = req.headers.token;
+  Streamer.findOne()
+    .where('pseudo').equals(streamer)
+    .where('token').equals(token + 'toto')
+    .exec((err, streamer) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      if (!streamer) {
+        // Le streamer n'a pas été trouvé avec ce token ou avec ce pseudo
+        return res.status(400).json({code: 404, message: 'Streamer non trouvé'});
+      }
+      next();
+    });
+};
+
 const login = (req, res, next) => {
-  console.log(req.body.password);
   Streamer
     .findOne()
     .where('password').equals(req.body.password)
@@ -36,10 +53,29 @@ const login = (req, res, next) => {
 }
 
 const logout = (req, res, next) => {
-
+  Streamer
+    .findOne()
+    .where('pseudo').equals(req.headers.streamer)
+    .exec((err, streamer) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      if (!streamer) {
+        return res.status(400).json({code: 404, message: 'Streamer non trouvé'});
+      }
+      streamer.token = undefined;
+      streamer.save(err => {
+        console.log('test');
+        if (err) {
+          return res.status(500).json(err);
+        }
+        return res.send(204);
+      });
+    });
 }
 
 module.exports = {
   login,
   logout,
+  checkAuthentication,
 };
--- app.js
+++ app.js
@@ -45,12 +45,11 @@ app.use(express.static(__dirname + '/public'));
 //app.use(viewRoute);
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, contenttype, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, Token, Streamer, X-Requested-With, contenttype, Content-Type, Accept");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-
 
 // Use our api routes
 app.use('/api', streamerRoutes, slotRoutes, dayRoutes, weekRoutes, authenticationRoutes);
